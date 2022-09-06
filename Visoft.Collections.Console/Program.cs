7b647044f7191126fe010eb883c6b7df912cb883
Bruno Viotto <brunoviottosantos@gmail.com>
2022-09-06 11:09:45 -0300

    Alteração leve irrelevante

--- Visoft.Collections.Console/Program.cs
+++ Visoft.Collections.Console/Program.cs
@@ -8,9 +8,6 @@ public static class Program
 {
     static void Main()
     {
-        int[] arr = new int[10];
-        List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-        list.CopyTo(arr);
-        // BenchmarkRunner.Run<SetBenchmark>();
+        BenchmarkRunner.Run<SetBenchmark>();
     }
 }
